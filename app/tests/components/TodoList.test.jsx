a883d545649fb71afbfb07f3999d85e927e2739f
Werner Emmanuel Guandique <werner@haegis.io>
2017-01-18 10:08:07 -0500

    Added tests

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,26 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var expect = require("expect");
+var $ = require("jQuery");
+var TestUtils = require("react-addons-test-utils");
+
+var TodoList = require("TodoList");
+var Todo = require("Todo");
+
+describe("TodoList", () => {
+  it("should exist", () => {
+    expect(TodoList).toExist();
+  });
+
+  it("should render one Todo component for each Todo item", () => {
+    var todos = [
+      {id: 1, text: "Do something"},
+      {id: 2, text: "Do something else"}
+    ];
+
+    var renderedTodoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    var renderedTodoComponents = TestUtils.scryRenderedComponentsWithType(renderedTodoList, Todo);
+
+    expect(renderedTodoComponents.length).toBe(todos.length);
+  });
+});
