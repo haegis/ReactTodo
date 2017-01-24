a3e33f52bb02134d8104f264f446a1802c4aa493
Werner Emmanuel Guandique <werner@haegis.io>
2017-01-24 11:30:06 -0500

    Added redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
 
 var Main = require("Main");
+var actions = require("actions");
+var store = require("configureStore").configure();
 
 //Load foundation
 require("style!css!foundation-sites/dist/css/foundation.min.css");
--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,13 @@ module.exports = {
     modulesDirectories: [
       "node_modules",
       "./app/components",
-      "./app/api",
-      "./app/actions",
-      "./app/reducers"
+      "./app/api"
     ],
     alias: {
-      applicationStyles: "app/styles/app.scss"
+      applicationStyles: "app/styles/app.scss",
+      actions: "app/actions/actions.jsx",
+      reducers: "app/reducers/reducers.jsx",
+      configureStore: "app/store/configureStore.jsx"
     },
     extensions: ["", ".js", ".jsx"]
   },
